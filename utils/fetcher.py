c1f63b5483bec61137dc4693f6589b9fa3495687
HaaaE <zwl3170868212@outlook.com>
2025-12-17 21:19:26 +0800

    fix ocr package problem

--- utils/fetcher.py
+++ utils/fetcher.py
@@ -7,7 +7,7 @@ import logging
 from pathlib import Path
 import sys, os
 sys.path.insert(0, str(Path(__file__).resolve().parent.parent))
-from ocr import ocr  # 导入自定义OCR模块
+import ocr  # 导入自定义OCR模块
 
 # --- 配置与常量 ---
 BASE_URL = "https://jwc.swjtu.edu.cn"
